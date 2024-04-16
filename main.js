92e104414ca142f6f3ee81f85fa49208a0aab75a
Myles <myles.sanigar@gmail.com>
2024-04-16 16:33:15 +0100

    turn again

--- main.js
+++ main.js
@@ -11,9 +11,29 @@ class VideoChat {
     this.servers = {
       iceServers: [
         {
-          urls: ['STUN:stun.cloudflare.com:3478', 'stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
-        }
-      ]
+          urls: "stun:stun.relay.metered.ca:80",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80",
+          username: "5a0fbfecdc7ac35f2d2706da",
+          credential: "v8RfUDfGLXn1Ji7q",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80?transport=tcp",
+          username: "5a0fbfecdc7ac35f2d2706da",
+          credential: "v8RfUDfGLXn1Ji7q",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:443",
+          username: "5a0fbfecdc7ac35f2d2706da",
+          credential: "v8RfUDfGLXn1Ji7q",
+        },
+        {
+          urls: "turns:global.relay.metered.ca:443?transport=tcp",
+          username: "5a0fbfecdc7ac35f2d2706da",
+          credential: "v8RfUDfGLXn1Ji7q",
+        },
+      ],
     };
     this.constraints = {
       video: {
