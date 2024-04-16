1606f1db811066f5333fa615e5c08dd86aa46db2
Myles <myles.sanigar@gmail.com>
2024-04-16 16:04:23 +0100

    ice ice baby

--- main.js
+++ main.js
@@ -11,13 +11,10 @@ class VideoChat {
     this.servers = {
       iceServers: [
         {
-          urls: "stun:stun.l.google.com:19302",
-        },
-        {
-          urls: "stun:stun1.l.google.com:19302",
-        },
-        {
-          urls: "stun:stun2.l.google.com:19302",
+          urls: [
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+          ],
         },
       ],
     };
