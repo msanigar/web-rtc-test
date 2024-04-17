67e26dfa5b4b21c683f6b73effbe4dead63acf34
Myles <myles.sanigar@gmail.com>
2024-04-17 20:42:54 +0100

    Complete refactor
    Use Agora RTC rather than messing with ICECandidates, STUN & TURN servers etc. etc.
    
    MUCH EASIER!

---
+++ js/room_rtc.js
@@ -0,0 +1,209 @@
+const APP_ID = "05e0a4c74bfb4211ab5afb2d41b25691";
+
+let uid = sessionStorage.getItem("uid");
+if (!uid) {
+  uid = String(Math.floor(Math.random() * 10000));
+  sessionStorage.setItem("uid", uid);
+}
+
+let token = null;
+let client;
+
+let rtmClient;
+let channel;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get("room");
+
+if (!roomId) {
+  roomId = "main";
+}
+
+let displayName = sessionStorage.getItem("display_name");
+if (!displayName) {
+  window.location = "lobby.html";
+}
+
+let localTracks = [];
+let remoteUsers = {};
+
+let joinRoomInit = async () => {
+  rtmClient = await AgoraRTM.createInstance(APP_ID);
+  await rtmClient.login({ uid, token });
+
+  await rtmClient.addOrUpdateLocalUserAttributes({ name: displayName });
+
+  channel = await rtmClient.createChannel(roomId);
+  await channel.join();
+
+  channel.on("MemberJoined", handleMemberJoined);
+  channel.on("MemberLeft", handleMemberLeft);
+  channel.on("ChannelMessage", handleChannelMessage);
+
+  getMembers();
+  addBotMessageToDom(`Welcome to the room ${displayName}! 👋`);
+
+  client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+  await client.join(APP_ID, roomId, token, uid);
+
+  joinStream();
+
+  client.on("user-published", handleUserPublished);
+  client.on("user-left", handleUserLeft);
+};
+
+let joinStream = async () => {
+  document.getElementsByClassName("stream__actions")[0].style.display = "flex";
+
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks(
+    {},
+    {
+      encoderConfig: {
+        width: { min: 640, ideal: 1920, max: 1920 },
+        height: { min: 480, ideal: 1080, max: 1080 },
+      },
+    }
+  );
+
+  let player = `<div class="video__container" id="user-container-${uid}">
+                    <div class="video-player" id="user-${uid}"></div>
+                 </div>`;
+
+  document
+    .getElementById("streams__container")
+    .insertAdjacentHTML("beforeend", player);
+  document
+    .getElementById(`user-container-${uid}`)
+    .addEventListener("click", expandVideoFrame);
+
+  localTracks[1].play(`user-${uid}`);
+  await client.publish([localTracks[0], localTracks[1]]);
+};
+
+let switchToCamera = async () => {
+  let player = `<div class="video__container" id="user-container-${uid}">
+                    <div class="video-player" id="user-${uid}"></div>
+                 </div>`;
+  displayFrame.insertAdjacentHTML("beforeend", player);
+
+  await localTracks[0].setMuted(true);
+  await localTracks[1].setMuted(true);
+
+  document.getElementById("mic-btn").classList.remove("active");
+
+  localTracks[1].play(`user-${uid}`);
+  await client.publish([localTracks[1]]);
+};
+
+let handleUserPublished = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+
+  await client.subscribe(user, mediaType);
+
+  let player = document.getElementById(`user-container-${user.uid}`);
+  if (player === null) {
+    player = `<div class="video__container" id="user-container-${user.uid}">
+                <div class="video-player" id="user-${user.uid}"></div>
+            </div>`;
+
+    document
+      .getElementById("streams__container")
+      .insertAdjacentHTML("beforeend", player);
+    document
+      .getElementById(`user-container-${user.uid}`)
+      .addEventListener("click", expandVideoFrame);
+  }
+
+  if (displayFrame.style.display) {
+    let videoFrame = document.getElementById(`user-container-${user.uid}`);
+    videoFrame.style.height = "100px";
+    videoFrame.style.width = "100px";
+  }
+
+  if (mediaType === "video") {
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
+
+let handleUserLeft = async (user) => {
+  delete remoteUsers[user.uid];
+  let item = document.getElementById(`user-container-${user.uid}`);
+  if (item) {
+    item.remove();
+  }
+
+  if (userIdInDisplayFrame === `user-container-${user.uid}`) {
+    displayFrame.style.display = null;
+
+    let videoFrames = document.getElementsByClassName("video__container");
+
+    for (let i = 0; videoFrames.length > i; i++) {
+      videoFrames[i].style.height = "300px";
+      videoFrames[i].style.width = "300px";
+    }
+  }
+};
+
+let toggleMic = async (e) => {
+  let button = e.currentTarget;
+
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false);
+    button.classList.add("active");
+  } else {
+    await localTracks[0].setMuted(true);
+    button.classList.remove("active");
+  }
+};
+
+let toggleCamera = async (e) => {
+  let button = e.currentTarget;
+
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false);
+    button.classList.add("active");
+  } else {
+    await localTracks[1].setMuted(true);
+    button.classList.remove("active");
+  }
+};
+
+let leaveStream = async (e) => {
+  e.preventDefault();
+
+  document.getElementsByClassName("stream__actions")[0].style.display = "none";
+
+  for (let i = 0; localTracks.length > i; i++) {
+    localTracks[i].stop();
+    localTracks[i].close();
+  }
+
+  await client.unpublish([localTracks[0], localTracks[1]]);
+
+  document.getElementById(`user-container-${uid}`).remove();
+
+  if (userIdInDisplayFrame === `user-container-${uid}`) {
+    displayFrame.style.display = null;
+
+    for (let i = 0; videoFrames.length > i; i++) {
+      videoFrames[i].style.height = "300px";
+      videoFrames[i].style.width = "300px";
+    }
+  }
+
+  channel.sendMessage({
+    text: JSON.stringify({ type: "user_left", uid: uid }),
+  });
+  window.location = "lobby.html";
+};
+
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("leave-btn").addEventListener("click", leaveStream);
+
+joinRoomInit();
