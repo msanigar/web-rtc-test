345cb620ae02b2e3322fb5aaf6a09e5e9c8105e2
Myles <myles.sanigar@gmail.com>
2024-04-17 20:44:30 +0100

    renaming index was dumb

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -22,7 +22,7 @@ if (!roomId) {
 
 let displayName = sessionStorage.getItem("display_name");
 if (!displayName) {
-  window.location = "lobby.html";
+  window.location = "index.html";
 }
 
 let localTracks = [];
@@ -199,7 +199,7 @@ let leaveStream = async (e) => {
   channel.sendMessage({
     text: JSON.stringify({ type: "user_left", uid: uid }),
   });
-  window.location = "lobby.html";
+  window.location = "index.html";
 };
 
 document.getElementById("camera-btn").addEventListener("click", toggleCamera);
